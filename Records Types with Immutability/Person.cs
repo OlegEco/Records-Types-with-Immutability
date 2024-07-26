540c6c00730ef23b5ec73db03e04a8bb844dbd7f
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-26 09:15:07 +0200

    Add project files.

---
+++ Records Types with Immutability/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Records_Types_with_Immutability
+{
+    public record class Person(string name, int age)
+    {
+        public string Adress { get; init; } = "Unknown";
+    }
+}
---
+++ Records Types with Immutability/Program.cs
@@ -0,0 +1,4 @@
+﻿using Records_Types_with_Immutability;
+
+var person = new Person("John", 30) { Adress = "123 Main St" };
+Console.WriteLine($"Name: {person.name}, Age: {person.age}, Adress: {person.Adress}");
